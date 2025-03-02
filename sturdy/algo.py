a1ac1c09e033c47c958c3c82c7b1bda9dbbe591e
LordOfCodes26 <LordOfCodes1026@gmail.com>
2025-03-02 00:02:33 -0800

    add supply_rate_dict

--- sturdy/algo.py
+++ sturdy/algo.py
@@ -19,7 +19,6 @@ THRESHOLD = 0.99  # used to avoid over-allocations
 def naive_algorithm(self: BaseMinerNeuron, synapse: AllocateAssets) -> dict:
     bt.logging.debug(f"received request type: {synapse.request_type}")
     pools = cast(dict, synapse.assets_and_pools["pools"])
-    bt.logging.debug(f"before_created_pools: {pools}")
     for uid, pool in pools.items():
         pools[uid] = PoolFactory.create_pool(
             pool_type=pool.pool_type,
@@ -29,29 +28,22 @@ def naive_algorithm(self: BaseMinerNeuron, synapse: AllocateAssets) -> dict:
             ),  # TODO: is there a cleaner way to do this?
             contract_address=pool.contract_address,
         )
-    bt.logging.debug(f"created_pools: {pools}")
     total_assets_available = int(THRESHOLD * synapse.assets_and_pools["total_assets"])
-    bt.logging.debug(f"total_assets_available: {total_assets_available}")
     pools = cast(dict, synapse.assets_and_pools["pools"])
-    bt.logging.debug(f"casted pools: {pools}")
     supply_rate_sum = 0
     supply_rates = {}
 
     # sync pool parameters by calling smart contracts on chain
     for pool in pools.values():
         pool.sync(self.w3)
-    bt.logging.debug(f"PoolFactory output: {pools}")
 
     # check the amounts that have been borrowed from the pools - and account for them
     minimums = {}
     for pool_uid, pool in pools.items():
         minimums[pool_uid] = get_minimum_allocation(pool)
-    bt.logging.debug(f"minimums: {minimums}")
     total_assets_available -= sum(minimums.values())
-    bt.logging.debug(f"total_assets_available_minus_miniums: {total_assets_available}")
     balance = int(total_assets_available)  # obtain supply rates of pools - aave pool and sturdy silo
     # rates are determined by making on chain calls to smart contracts
-    bt.logging.debug(f"balance: {balance}")
     for pool in pools.values():
         bt.logging.debug(f"pool_contract_address: {pool.contract_address}")
         match pool.pool_type:
@@ -65,7 +57,33 @@ def naive_algorithm(self: BaseMinerNeuron, synapse: AllocateAssets) -> dict:
                 supply_rates[pool.contract_address] = apy
                 bt.logging.debug(f"pool_apy: {apy}") 
                 supply_rate_sum += apy
-
+    supply_rate_dict = {
+        pool_uid: calculate_apy(pools[pool_uid], balance, pools) for pool_uid in pools
+    }
+    bt.logging.debug(f"supply_rate_dict: {supply_rate_dict}")
     return {
         pool_uid: minimums[pool_uid] + math.floor((supply_rates[pool_uid] / supply_rate_sum) * balance) for pool_uid in pools
     }
+
+def calculate_apy(pool, balance: int, pools: dict) -> int:
+    """
+    Calculate the Annual Percentage Yield (APY) for a given pool.
+
+    Parameters:
+    - pool: The pool object containing the supply rate method.
+    - balance: The total balance available for investment.
+    - pools: A list of pools to be considered for the calculation.
+
+    Returns:
+    - APY as a int.
+    """
+    if not pools:
+        raise ValueError("The pools dict cannot be empty.")
+    
+    match pool.pool_type:
+        case POOL_TYPES.DAI_SAVINGS:
+            apy = pool.supply_rate()
+        case _:
+            apy = pool.supply_rate(balance // len(pools))
+    
+    return apy
